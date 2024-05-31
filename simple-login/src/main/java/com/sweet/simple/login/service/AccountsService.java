80b27ad7c1612e2f58c97bb329e6bbf35c4775e2
大师兄 <767898866@qq.com>
2024-05-31 22:08:02 +0800

    #

--- simple-login/src/main/java/com/sweet/simple/login/service/AccountsService.java
+++ simple-login/src/main/java/com/sweet/simple/login/service/AccountsService.java
@@ -5,6 +5,11 @@ import com.sweet.simple.login.entity.Accounts;
 import com.sweet.simple.login.mapper.AccountsMapper;
 import org.springframework.stereotype.Service;
 
+/**
+ * 玩家账号服务
+ *
+ * @author 大师兄
+ */
 @Service
 public class AccountsService extends ServiceImpl<AccountsMapper, Accounts> {
 
